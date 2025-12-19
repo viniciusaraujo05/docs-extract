6fb8d475caccc62db9633b194676ba7394a129bb
Vinicius araujo <vinicius.araujo-silva@vinci-energies.net>
2025-12-19 20:50:31 +0000

    update ph8.5

--- vite.config.ts
+++ vite.config.ts
@@ -17,9 +17,9 @@ export default defineConfig({
             },
         }),
         tailwindcss(),
-        ...(process.env.NODE_ENV !== 'production' ? [wayfinder({
-            formVariants: true,
-        })] : []),
+        // ...(process.env.NODE_ENV !== 'production' ? [wayfinder({
+        //     formVariants: true,
+        // })] : []),
     ],
     esbuild: {
         jsx: 'automatic',
