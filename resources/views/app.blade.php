89c3c95ed83acba29ae03c476f42801508f52270
Vinicius araujo <vinicius.araujo-silva@vinci-energies.net>
2026-02-23 15:39:06 +0000

    feat: Add Apollo.io website tracker script.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -138,6 +138,20 @@
                 });
             </script>
             <script type="text/javascript" charset="UTF-8" src="//cdn.cookie-script.com/s/515ed2cba50c7a9fc9f575d03a7aa1df.js"></script>
+
+            {{-- Apollo.io Website Tracker --}}
+            <script>
+                function initApollo(){
+                    var n=Math.random().toString(36).substring(7),
+                        o=document.createElement("script");
+                    o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n;
+                    o.async=!0;
+                    o.defer=!0;
+                    o.onload=function(){window.trackingFunctions.onLoad({appId:"699c707353bb4b0015c49db3"})};
+                    document.head.appendChild(o)
+                }
+                initApollo();
+            </script>
         @endproduction
         @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
